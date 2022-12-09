d9442012d1051b7cfeee105442780c401588ed6d
m-wynne <michael.d.wynne19@gmail.com>
2022-12-10 10:30:42 +1100

    finish key functions

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 console.log('Hello!');
 
-const introPhrases = [];
+const introPhrases = ['Congratulations', ''];
 
 const nounPhrases = [];
 
 const verbPhrases = [];
+
+const selectPhrases = (introPhrases, nounPhrases, verbPhrases) => {
+    //Randomly choose one element from each input array
+    const rng1 = Math.floor(Math.random() * introPhrases.length);
+    const rng2 = Math.floor(Math.random() * nounPhrases.length);
+    const rng3 = Math.floor(Math.random() * verbPhrases.length);
+};
+
+const printMessage = () => {
+    //Call selectPhrases(), turn the phrases into a message, then log result to the console
+};
