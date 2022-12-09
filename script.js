098f8052067c69770235166981c5805b68583f1a
m-wynne <michael.d.wynne19@gmail.com>
2022-12-10 10:37:04 +1100

    add 6 phrases for each array

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 console.log('Hello!');
 
-const introPhrases = ['Congratulations', ''];
+const introPhrases = ['Congratulations', 'My condolences', 'Success', 'Uh-oh', 'Time to worry', 'Countdown has begun'];
 
-const nounPhrases = [];
+const nounPhrases = ['Thrusters', 'Oxygen supply', 'Life support', 'Airlock', 'Navigation system', 'Communication system'];
 
-const verbPhrases = [];
+const verbPhrases = ['terminated', 'shut down', 'restarted', 'never an issue', 'fixed', 'destroyed'];
 
 const selectPhrases = (introPhrases, nounPhrases, verbPhrases) => {
     //Randomly choose one element from each input array
     const rng1 = Math.floor(Math.random() * introPhrases.length);
     const rng2 = Math.floor(Math.random() * nounPhrases.length);
     const rng3 = Math.floor(Math.random() * verbPhrases.length);
+    return [introPhrases[rng1], nounPhrases[rng2], verbPhrases[rng3]];
 };
 
-const printMessage = () => {
+const printMessage = (introPhrases, nounPhrases, verbPhrases) => {
     //Call selectPhrases(), turn the phrases into a message, then log result to the console
+    const phrases = selectPhrases(introPhrases, nounPhrases, verbPhrases);
+    console.log(`${phrases[0]}! ${phrases[1]} was ${phrases[2]}.`);
 };
